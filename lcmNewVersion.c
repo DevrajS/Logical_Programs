c754e9f26549efbc2e76937c31027c2cbab02805
DevRaj Sharma <neeraj271195@gmail.com>
2018-07-21 01:25:34 +0530

    version 1.0

---
+++ lcmNewVersion.c
@@ -0,0 +1,35 @@
+/*
+ -| DevRaj |
+
+Program to calculate LCM (Least common multiple) of two numbers
+
+Explain :
+ Lcm means smallest common multiple of numbers may be two or more.
+
+ For eg :  The smallest number which can be divisible by all the given numbers
+ than the number will be lcm of those given numbers.
+ 
+*/
+
+#include<stdio.h>
+int lcm(int num1,int num2)
+{
+  
+	for (int lcm = 1;lcm <=num2*num1 ; lcm++) // in worst case it will run till num1*num2
+	{
+		if (lcm%num1==0 && lcm %num2==0)
+		{
+			return lcm;
+		}
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	int lcm,num1,num2;
+	printf("Enter number");
+	scanf("%d %d",num1,num2);
+    result=lcm(num1,num2);
+    printf("Lcm = %d \n",lcm);
+	return 0;
+}
